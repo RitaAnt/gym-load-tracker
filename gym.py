f87f47299daca4f754a88d0354a4821211f08192
Rita M <kolotvinova03@bk.ru>
2026-05-14 09:53:18 +0300

    Update gym.py
    moscow UTC

--- gym.py
+++ gym.py
@@ -1,6 +1,6 @@
 import requests
 import csv
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 import os
 
 url = "https://mobifitness.ru/api/v8/clubs/4573.json"
@@ -15,8 +15,11 @@ data = requests.get(url, headers=headers).json()
 
 load = int(data["currentLoad"])
 
+moscow_tz = timezone(timedelta(hours=3))
+current_time = datetime.now(moscow_tz)
+
 with open("gym.csv", "a", newline="") as f:
     writer = csv.writer(f)
-    writer.writerow([datetime.now(), load])
+    writer.writerow([current_time, load])
 
 print("Saved:", load)
